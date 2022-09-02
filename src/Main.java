64b8cc50dc79a89a5c10019a62a1cd0bff0fd8e0
Макс Апаков <maksapakov@gmail.com>
2022-09-02 15:04:59 +0300

    ver.0.51

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,178 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        // Random variables
+        String randomFrom = "Илон Маск"; // Некоторая случайная строка. Можете выбрать ее самостоятельно.
+        String randomTo = "Максим Евгеньевич";  // Некоторая случайная строка. Можете выбрать ее самостоятельно.
+        int randomSalary = 100;  // Некоторое случайное целое положительное число. Можете выбрать его самостоятельно.
+
+// Создание списка из трех почтовых сообщений.
+        MailMessage firstMessage = new MailMessage(
+                "Robert Howard",
+                "H.P. Lovecraft",
+                "This \"The Shadow over Innsmouth\" story is real masterpiece, Howard!"
+        );
+
+        assert firstMessage.getFrom().equals("Robert Howard") : "Wrong firstMessage from address";
+        assert firstMessage.getTo().equals("H.P. Lovecraft") : "Wrong firstMessage to address";
+        assert firstMessage.getContent().endsWith("Howard!") : "Wrong firstMessage content ending";
+
+        MailMessage secondMessage = new MailMessage(
+                "Jonathan Nolan",
+                "Christopher Nolan",
+                "Брат, почему все так хвалят только тебя, когда практически все сценарии написал я. Так не честно!"
+        );
+
+        MailMessage thirdMessage = new MailMessage(
+                "Stephen Hawking",
+                "Christopher Nolan",
+                "Я так и не понял Интерстеллар."
+        );
+
+        List<MailMessage> messages = Arrays.asList(
+                firstMessage, secondMessage, thirdMessage
+        );
+
+// Создание почтового сервиса.
+        MailService<String> mailService = new MailService<>();
+
+// Обработка списка писем почтовым сервисом
+        messages.stream().forEachOrdered(mailService);
+
+// Получение и проверка словаря "почтового ящика",
+//   где по получателю можно получить список сообщений, которые были ему отправлены
+        Map<String, List<String>> mailBox = mailService.getMailBox();
+
+        assert mailBox.get("H.P. Lovecraft").equals(
+                Arrays.asList(
+                        "This \"The Shadow over Innsmouth\" story is real masterpiece, Howard!"
+                )
+        ) : "wrong mailService mailbox content (1)";
+
+        assert mailBox.get("Christopher Nolan").equals(
+                Arrays.asList(
+                        "Брат, почему все так хвалят только тебя, когда практически все сценарии написал я. Так не честно!",
+                        "Я так и не понял Интерстеллар."
+                )
+        ) : "wrong mailService mailbox content (2)";
+
+        assert mailBox.get(randomTo).equals(Collections.<String>emptyList()) : "wrong mailService mailbox content (3)";
+
+
+// Создание списка из трех зарплат.
+        Salary salary1 = new Salary("Facebook", "Mark Zuckerberg", 1);
+        Salary salary2 = new Salary("FC Barcelona", "Lionel Messi", Integer.MAX_VALUE);
+        Salary salary3 = new Salary(randomFrom, randomTo, randomSalary);
+
+// Создание почтового сервиса, обрабатывающего зарплаты.
+        MailService<Integer> salaryService = new MailService<>();
+
+// Обработка списка зарплат почтовым сервисом
+        Arrays.asList(salary1, salary2, salary3).forEach(salaryService);
+
+// Получение и проверка словаря "почтового ящика",
+//   где по получателю можно получить список зарплат, которые были ему отправлены.
+        Map<String, List<Integer>> salaries = salaryService.getMailBox();
+        assert salaries.get(salary1.getTo()).equals(Arrays.asList(1)) : "wrong salaries mailbox content (1)";
+        assert salaries.get(salary2.getTo()).equals(Arrays.asList(Integer.MAX_VALUE)) : "wrong salaries mailbox content (2)";
+        assert salaries.get(randomTo).equals(Arrays.asList(randomSalary)) : "wrong salaries mailbox content (3)";
+    }
+
+    ////////////////Код Начало////////////
+    public static interface Sendable<T> {
+        String getFrom();
+        String getTo();
+        T getContent();
+    }
+    public static class SimpleSendable<T> implements Sendable<T> {
+        private String from, to;
+        private T content;
+
+        public SimpleSendable(String from, String to, T content) {
+            this.from = from;
+            this.to = to;
+            this.content = content;
+        }
+
+        @Override
+        public String getFrom() {
+            return from;
+        }
+
+        @Override
+        public String getTo() {
+            return to;
+        }
+
+        @Override
+        public T getContent() {
+            return content;
+        }
+    }
+    public static class MailMessage extends SimpleSendable implements Sendable{
+        String from, to;
+        Object content;
+        public MailMessage(String from, String to, Object content) {
+            super(from, to, content);
+            this.from = from;
+            this.to = to;
+            this.content = content;
+        }
+
+        @Override
+        public String getFrom() {
+            return from;
+        }
+
+        @Override
+        public String getTo() {
+            return to;
+        }
+
+        @Override
+        public String getContent() {
+            return content.toString();
+        }
+    }
+
+    public static class Salary extends SimpleSendable implements Sendable {
+        String from, to;
+        Object content;
+        public Salary(String from, String to, Object content) {
+            super(from, to, content);
+            this.from = from;
+            this.to = to;
+            this.content = content;
+        }
+
+        @Override
+        public String getFrom() {
+            return from;
+        }
+
+        @Override
+        public String getTo() {
+            return to;
+        }
+
+        @Override
+        public Integer getContent() {
+            return (Integer) content;
+        }
+        // implement here
+    }
+
+    public static class MailService implements {
+
+        public Map<?, ?> getMailBox() {
+            return null;
+        }
+
+        // implement here
+    }
 }
